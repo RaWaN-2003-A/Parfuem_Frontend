c9b8b5c02c2c900ab1946f4fb1e015067968912d
RaWaN-2003-A <Rawan.Alhussin@Student.HTW-Berlin.de>
2026-05-16 23:15:06 +0200

    feat: successfully launch Angular frontend on local dev server and configure routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
-
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component'; // إذا كنتِ تستخدمين الفوتر أيضاً
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ],//Wir haben die Parfüm-Liste in die offizielle Gästeliste (imports) der Haupt-App eingetragen
+  // السر كله في هذا السطر: يجب أن نضع المكونات هنا ليتعرف عليها الـ HTML
+  imports: [RouterOutlet, NavComponent, FooterComponent], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
