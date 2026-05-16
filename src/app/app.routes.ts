092c1af0fab9aa843628781c25d447692297298c
RaWaN-2003-A <Rawan.Alhussin@Student.HTW-Berlin.de>
2026-05-16 19:28:39 +0200

    bearbeitung

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { TableComponent } from './table/table';
-import { CreateComponent } from './create/create';
-import { DetailComponent } from './detail/detail';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+import { CreateComponent } from './create/create.component';
+import { DetailComponent } from './detail/detail.component';
 
 export const routes: Routes = [{
     path: "",
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-table',
   imports: [],
-  templateUrl: './table.html',
-  styleUrl: './table.css',
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css',
 })
 export class Table {
 
