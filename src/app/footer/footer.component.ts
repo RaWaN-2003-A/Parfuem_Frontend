51aa0a9c21a4504d52149b046d9e3a33d0fe17dd
RaWaN-2003-A <Rawan.Alhussin@Student.HTW-Berlin.de>
2026-06-03 20:18:22 +0200

    Footer design Fertig

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // <-- WICHTIG für die Links im Footer es soll importiert werden, damit die RouterLink Direktive in der Footer-Komponente verwendet werden kann
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], // <-- Hier hinzufügen
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
-  
+  styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
