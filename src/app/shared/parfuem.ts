de7417f6cbb7924a1b8310e727df1cd524ade869
RaWaN-2003-A <Rawan.Alhussin@Student.HTW-Berlin.de>
2026-06-05 14:33:28 +0200

    interface parfuem.ts aktuallisiert

--- src/app/shared/parfuem.ts
+++ src/app/shared/parfuem.ts
@@ -3,4 +3,9 @@ export interface Parfuem {
   name: string;     // اسم العطر
   marke: string;    // الماركة
   preis: number;    // السعر
+  katoegorie? : string; // الفئة (اختياري)
+  beschreibung?: string; // الوصف (اختياري)
+  bestandt?: number; // الكمية المتوفرة (اختياري)
+  bildUrl?: string; // رابط الصورة (اختياري)
+  featured?: boolean; // هل العطر مميز؟ (اختياري)
 }
