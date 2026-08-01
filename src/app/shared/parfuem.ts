72c8d320339228cecdce489c25397a2ceaa169fd
RaWaN-2003-A <Rawan.Alhussin@Student.HTW-Berlin.de>
2026-08-01 17:36:51 +0200

    Die großer der Hero Video bearbeitet

--- src/app/shared/parfuem.ts
+++ src/app/shared/parfuem.ts
@@ -1,12 +1,15 @@
 export interface Parfuem {
-  _id?: string;     // معرف العطر في قاعدة البيانات
-  name: string;     // اسم العطر
-  marke: string;    // الماركة
-  preis: number;    // السعر
-  kategorie?: string; // الفئة
+  _id?: string;     // ereignis-ID (optional, da sie von der Datenbank generiert wird)
+  name: string;     // name des Parfüms 
+  marke: string;    // marke des Parfüms
+  preis: number;    // preis des Parfüms
+  kategorie?: string; // kategorie des Parfüms (optional)
   beschreibung?: string; // الوصف
   bestand?: number; // الكمية المتوفرة
   bildUrl?: string; // رابط الصورة 
   featured?: boolean; // هل العطر مميز؟ 
   groesse?: string; // حجم العطر 
+
+  // Um felher bei der Datenübertragung zu vermeiden --> interface definieren, damit weiß Angular
+  // welche Felder erwartet werden und welche optional sind.
 }
